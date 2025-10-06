016223a5d3b123df305a646a020470017f812e35
Michal Urbanek <michal.urbanek@strv.com>
2025-10-06 13:13:43 +0200

    fix: Remove custom page transition that can be replaced with default one

--- lib/features/home/home_page_content.dart
+++ lib/features/home/home_page_content.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app/navigation/app_router.dart';
-import 'package:flutter_app/common/animation/custom_fade_slide_animation.dart';
 import 'package:flutter_app/common/component/custom_button/custom_button_primary.dart';
 import 'package:flutter_app/common/component/custom_text/custom_text.dart';
 import 'package:flutter_app/common/extension/build_context.dart';
@@ -18,22 +17,16 @@ class HomePageContent extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CustomFadeSlideAnimation(
-                delay: const Duration(milliseconds: 300),
-                child: CustomText(
-                  key: const Key('home_title'), // Used for Integration testing
-                  text: 'Home',
-                  style: context.textTheme.titleMedium,
-                  textAlign: TextAlign.center,
-                ),
+              CustomText(
+                key: const Key('home_title'), // Used for Integration testing
+                text: 'Home',
+                style: context.textTheme.titleMedium,
+                textAlign: TextAlign.center,
               ),
               const SizedBox(height: 16),
-              CustomFadeSlideAnimation(
-                delay: const Duration(milliseconds: 600),
-                child: CustomButtonPrimary(
-                  onPressed: () => context.pushRoute(const DebugToolsRoute()),
-                  text: context.locale.featureHomepageOpenDebugTools,
-                ),
+              CustomButtonPrimary(
+                onPressed: () => context.pushRoute(const DebugToolsRoute()),
+                text: context.locale.featureHomepageOpenDebugTools,
               ),
             ],
           ),
