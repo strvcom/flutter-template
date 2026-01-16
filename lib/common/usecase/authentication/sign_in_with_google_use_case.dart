45a81130bc973a8883f97f317df80acf95b92619
Michal Urbanek <michal.urbanek@strv.com>
2026-01-16 11:58:05 +0100

    refactor: Sign in Google and Apple

--- lib/common/usecase/authentication/sign_in_with_google_use_case.dart
+++ lib/common/usecase/authentication/sign_in_with_google_use_case.dart
@@ -1,40 +1,49 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_app/common/data/entity/user_entity.dart';
-import 'package:flutter_app/common/usecase/authentication/sign_in_with_auth_credential_use_case.dart';
+import 'package:flutter_app/common/data/entity/exception/custom_exception.dart';
 import 'package:flutter_app/core/flogger.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'sign_in_with_google_use_case.g.dart';
 
 const List<String> _scopes = <String>[
   'email',
 ];
 
-final signInWithGoogleUseCase = FutureProvider<UserEntity>((ref) async {
-  Flogger.d('[Authentication] Sign in with Google started');
+@riverpod
+Future<void> signInWithGoogleUseCase(Ref ref) async {
+  try {
+    Flogger.d('[Authentication] Sign in with Google started');
 
-  final googleSignIn = GoogleSignIn.instance;
+    final googleSignIn = GoogleSignIn.instance;
 
-  // Subtitle: Step 1 - Logout from Current Google account if any
-  await googleSignIn.disconnect();
+    // Subtitle: Step 1 - Logout from Current Google account if any
+    await googleSignIn.disconnect();
 
-  // Subtitle: Step 2 - Authenticate user with Google
-  final account = await googleSignIn.authenticate();
+    // Subtitle: Step 2 - Authenticate user with Google
+    final account = await googleSignIn.authenticate();
 
-  // Subtitle: Step 3 - Authorize scopes
-  var authorization = await account.authorizationClient.authorizationForScopes(_scopes);
-  authorization ??= await account.authorizationClient.authorizeScopes(_scopes);
+    // Subtitle: Step 3 - Authorize scopes
+    var authorization = await account.authorizationClient.authorizationForScopes(_scopes);
+    authorization ??= await account.authorizationClient.authorizeScopes(_scopes);
 
-  // Subtitle: Step 4 - Create OAuth credential for Firebase
-  final oauthCredential = GoogleAuthProvider.credential(
-    accessToken: authorization.accessToken,
-    idToken: account.authentication.idToken,
-  );
+    // Subtitle: Step 4 - Create OAuth credential for Firebase
+    final oauthCredential = GoogleAuthProvider.credential(
+      accessToken: authorization.accessToken,
+      idToken: account.authentication.idToken,
+    );
 
-  Flogger.d('[Authentication] Received credential: $oauthCredential');
+    Flogger.d('[Authentication] Received credential: $oauthCredential');
 
-  return await ref.read(
-    signInWithAuthCredentialUseCaseProvider(
-      credential: oauthCredential,
-    ).future,
-  );
-});
+    // Subtitle: Step 5 - Sign in with credentials from provider
+    await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+  } on GoogleSignInException catch (e) {
+    if (e.code == GoogleSignInExceptionCode.canceled) {
+      throw const CustomException.signInCancelled();
+    } else {
+      throw CustomException.fromErrorObject(error: e);
+    }
+  } catch (e) {
+    throw CustomException.fromErrorObject(error: e);
+  }
+}
--- lib/features/authentication/authentication_event.dart
+++ lib/features/authentication/authentication_event.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_app/common/data/entity/exception/custom_exception.dart';
 import 'package:flutter_app/core/riverpod/event_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -7,6 +8,7 @@ part 'authentication_event.freezed.dart';
 @freezed
 sealed class AuthenticationEvent with _$AuthenticationEvent {
   const factory AuthenticationEvent.signedIn() = AuthenticationEventSignedIn;
+  const factory AuthenticationEvent.error(CustomException exception) = _Error;
 }
 
 final authenticationEventNotifierProvider = NotifierProvider.autoDispose<EventNotifier<AuthenticationEvent?>, AuthenticationEvent?>(
